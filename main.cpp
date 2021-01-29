0b3dfcb71936462947fdf96e3e21b6c116af9a03
Alexander Katrompas <alexander.katrompas@austincc.edu>
2021-01-29 14:46:54 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Replace this with your header file
+ */
+
+#include "main.h"
+
+int main(int argc, char** argv) {
+
+
+
+    return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,12 @@
+/*
+ * Replace this with your header file
+ */
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+
+#endif /* MAIN_H */
+
